73c78df66b7647df60d4e0d91734c1661c18bff9
tuxevara <tuxevara@f099d700-c32c-0410-a680-b7eb4ad3241d>
2007-03-30 14:23:53 +0000

    config inc created moved gettext settings to include file (very early state) template/simple directory created
    git-svn-id: https://svn.code.sf.net/p/elma/code/trunk@15 f099d700-c32c-0410-a680-b7eb4ad3241d

--- index.php
+++ index.php
@@ -6,26 +6,9 @@
 
 <?php
 
-// I18N support information here
-$language = 'en';
-putenv("LANG=$language"); 
-setlocale(LC_ALL, $language);
-
-// Set the text domain as 'messages'
-$domain = 'messages';
-bindtextdomain($domain, "locale"); 
-textdomain($domain);
-
-/* LDAP Servers Base DN */
-define ("LDAP_BASEDN","o=megabit");
-
-/* LDAP Servers Base DN for domains*/
-define ("LDAP_DOMAIN_ROOT_DN","ou=domains,".LDAP_BASEDN);
-
+require("includes/gettext.inc");
 require("includes/ldap_functions.inc");
 require("includes/my_functions.inc");
-
-
 
 
 include("includes/inst_checks.inc");
