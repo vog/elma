69886bb2d8c995142cbd1caf89f945bfceae1217
tuxevara <tuxevara@f099d700-c32c-0410-a680-b7eb4ad3241d>
2007-04-03 12:17:07 +0000

    renamed module skeleton remove double undescore from module.class constant APPROOT
    git-svn-id: https://svn.code.sf.net/p/elma/code/trunk@32 f099d700-c32c-0410-a680-b7eb4ad3241d

--- modules/content_main.php
+++ modules/content_main.php
@@ -1,28 +1,35 @@
 <?php
 /**
-* Content Module Main
-* 
-* This module displays the main page
-*
-* @author Rudolph Bott
-*
-*/
+ * Content Module Main
+ * 
+ * This module displays the main page
+ *
+ * @author Rudolph Bott <rbott@megabit.net>
+ * @author Daniel Weuthen <daniel@weuthen-net.de>
+ * @package elma
+ */
 
 class content_main extends module_base
 {
-    // Constructor of this class
+    /**
+     * Constructor of this class
+     */
 	function content_main() {
 	}
 
-    // This method is called after the constructor by the main page
+    /**
+     * This method is called after the constructor by the main page
+     */
 	function proceed() {
 
 	}
 
-	// This method returns any content that should be echoed by the
-    // main page.
-    //
-    // @return string
+    /**
+     * This method returns any content that should be echoed by the
+     * main page.
+     *
+     * @return string
+     */
 	function getContent() {
 	    $_content = $this->smarty->fetch('content_main.tpl');
    	    return $_content;
--- modules/modules.class.php
+++ modules/modules.class.php
@@ -1,30 +1,32 @@
 <?php
 /**
-* module factory
-*
-* This class contains a factory to call modules. This makes
-* calling modules by index.php much easier.
-*
-* $Id:: $
-* $LastChangedBy:: $
-* $LastChangedDate:: $
-* $LastChangedRevision:: $
-*/
+ * module factory
+ *
+ * This class contains a factory to call modules. This makes
+ * calling modules by index.php much easier.
+ *
+ * $Id:: $
+ * $LastChangedBy:: $
+ * $LastChangedDate:: $
+ * $LastChangedRevision:: $
+ *
+ */
 
 class modules {
     /**
-    * factory-method
-    *
-    * @param string type
-    * @return object class on success, error message on error
-    */
+     * factory-method
+     *
+     * @param string type
+     * @return object class on success, error message on error
+     *
+     */
     function &factory($type = "main") {  
-        if (!file_exists(__APPROOT__."/modules/content_${type}.php")) {   
+        if (!file_exists(APPROOT."/modules/content_${type}.php")) {   
             $type = "main";
         }
         $classname = "content_${type}";
 
-        include(__APPROOT__."/modules/${classname}.php");
+        include(APPROOT."/modules/${classname}.php");
 
         if ( !class_exists($classname) ) {   
 	    echo "error!";
@@ -36,14 +38,14 @@ class modules {
 }
 
 /**
-* Content Module Base Class
-* 
-* This is the base class for all content modules
-* All modules should be inherited from this class!
-*
-* @author Rudolph Bott
-*
-*/
+ * Content Module Base Class
+ * 
+ * This is the base class for all content modules
+ * All modules should be inherited from this class!
+ *
+ * @author Rudolph Bott
+ *
+ */
 
 class module_base {
     var $smarty;
@@ -52,25 +54,33 @@ class module_base {
     var $output = "";
     var $errors = "";
 
-    // Constructor of this class
-	function module_base() {
+    /**
+     * Constructor of this class
+     */
+    function module_base() {
 
 	}
 
-    // This method is called after the constructor by the main page
-	function proceed() {
+    /**
+     * This method is called after the constructor by the main page
+     */
+    function proceed() {
 
 	}
 
-	// return errors (if any)
+    /**
+     * return errors (if any)
+     */
 	function getErrors() {
 		return $errors;
 	}
 
-	// This method returns any content that should be echoed by the
-	// main page.
-    //
-    // @return string
+    /**
+     * This method returns any content that should be echoed by the
+     * main page.
+     *
+     * @return string
+     */
 	function getContent() {
     }
 }
