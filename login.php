3d3d9f1dfbb278a87a444526514cf44a1ffe28bb
killefiz <killefiz@f099d700-c32c-0410-a680-b7eb4ad3241d>
2007-04-12 17:37:44 +0000

    reformatted
    git-svn-id: https://svn.code.sf.net/p/elma/code/trunk@47 f099d700-c32c-0410-a680-b7eb4ad3241d

--- login.php
+++ login.php
@@ -9,33 +9,32 @@ session_start ();
 $ldap = new ELMA(LDAP_HOSTNAME);
 $ldap->connect();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $_SESSION["username"] = $_POST["username"];
-    if ( preg_match('/\@/',$_SESSION["username"]) ) {
+    if (preg_match('/\@/',$_SESSION["username"]) ) {
         list($local_part,$domain) = split("@",$_SESSION["username"]);
         $LDAP_BINDDN = "uid=$local_part,dc=$domain,".LDAP_DOMAINDN;
         $LDAP_BINDPASS = $_POST["password"];
-    } else if ( preg_match('/^admin$/',$_SESSION["username"]) ) {
+    } else if (preg_match('/^admin$/',$_SESSION["username"]) ) {
         $LDAP_BINDDN = "cn=admin,".LDAP_USERS_ROOT_DN;
         $LDAP_BINDPASS = $_POST["password"];
     }
   
-    if ( isset($LDAP_BINDDN) && isset($LDAP_BINDPASS) ) {
-	if ( $ldap->bind($LDAP_BINDDN,$LDAP_BINDPASS) == 0 ) {
+    if (isset($LDAP_BINDDN) && isset($LDAP_BINDPASS)) {
+        if ($ldap->bind($LDAP_BINDDN,$LDAP_BINDPASS) == 0) {
             $_SESSION["login"] = TRUE;
             $_SESSION["logintime"] = time();
-	    $_SESSION["username"] = $_POST["username"];
-	    $_SESSION["language"] = $_POST["language"];
+            $_SESSION["username"] = $_POST["username"];
+            $_SESSION["language"] = $_POST["language"];
 
-	    $crypt = new mycrypt();
-	    $_SESSION["ldap_binddn"] = $crypt->encrypt($LDAP_BINDDN);
-	    $_SESSION["ldap_bindpass"] = $crypt->encrypt($LDAP_BINDPASS);
-	
-	    header ("Location: index.php");
+            $crypt = new mycrypt();
+            $_SESSION["ldap_binddn"] = $crypt->encrypt($LDAP_BINDDN);
+            $_SESSION["ldap_bindpass"] = $crypt->encrypt($LDAP_BINDPASS);
+        
+            header ("Location: index.php");
             exit;
-	} else  {
-	    echo "error";
+        } else  {
+            echo "error";
             session_destroy();
         }
     }
