431d468a04f57352d1d54d62ac8a2099a67832ea
tuxevara <tuxevara@f099d700-c32c-0410-a680-b7eb4ad3241d>
2007-04-24 19:21:40 +0000

    added join parameter
    git-svn-id: https://svn.code.sf.net/p/elma/code/trunk@60 f099d700-c32c-0410-a680-b7eb4ad3241d

--- bin/gen_po.sh
+++ bin/gen_po.sh
@@ -4,4 +4,4 @@ TEMPLATE=$1
 LOCALE=$2
 
 tsmarty2c.php ../templates/$TEMPLATE/*.tpl > ../templates/$TEMPLATE/locale/$LOCALE/LC_MESSAGES/messages.c
-xgettext -C ../templates/$TEMPLATE/locale/$LOCALE/LC_MESSAGES/messages.c -o ../templates/$TEMPLATE/locale/$LOCALE/LC_MESSAGES/messages.po --keyword=_ --add-comments --from-code=utf-8
+xgettext -j -C ../templates/$TEMPLATE/locale/$LOCALE/LC_MESSAGES/messages.c -o ../templates/$TEMPLATE/locale/$LOCALE/LC_MESSAGES/messages.po --keyword=_ --add-comments --from-code=utf-8
