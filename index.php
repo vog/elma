3df6d88114384593c64bfc3ded2b21c09f371b9a
dweuthen <dweuthen@f099d700-c32c-0410-a680-b7eb4ad3241d>
2007-10-02 14:57:21 +0000

    corected include path for eximfilter.inc.php
    git-svn-id: https://svn.code.sf.net/p/elma/code/trunk@165 f099d700-c32c-0410-a680-b7eb4ad3241d

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ require("includes/gettext.inc.php");
 require("includes/ldap_functions.inc.php");
 require("includes/my_functions.inc.php");
 require("includes/crypt.inc.php");
-require("includes/sieve.inc.php");
+require("includes/eximfilter.inc.php");
 
 if (isset($_POST["module"])) 
     $module = $_POST["module"];
