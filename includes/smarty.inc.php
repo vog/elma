34f4a3d1ec16e4d521c68e929ecef458e43c619c
Panther_1 <Panther_1@f099d700-c32c-0410-a680-b7eb4ad3241d>
2007-12-07 13:50:59 +0000

    Activate cacheing
    git-svn-id: https://svn.code.sf.net/p/elma/code/trunk@196 f099d700-c32c-0410-a680-b7eb4ad3241d

--- includes/smarty.inc.php
+++ includes/smarty.inc.php
@@ -36,5 +36,6 @@ $smarty->php_handling = SMARTY_PHP_REMOVE;
 $smarty->assign('_SELF',$_SERVER['PHP_SELF']);
 $smarty->assign('template_path',"templates/".TEMPLATE);
 $smarty->compile_dir = getcwd().'/templates_c';
+$smarty->cache_dir = getcwd().'/cache';
 
 // vim:tabstop=4:expandtab:shiftwidth=4:filetype=php:syntax:ruler:
