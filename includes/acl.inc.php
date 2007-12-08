62d6153209512ee9b1af71ca7c7f55c912f87fc8
dweuthen <dweuthen@f099d700-c32c-0410-a680-b7eb4ad3241d>
2007-12-08 21:47:16 +0000

    added remarks
    git-svn-id: https://svn.code.sf.net/p/elma/code/trunk@206 f099d700-c32c-0410-a680-b7eb4ad3241d

--- includes/acl.inc.php
+++ includes/acl.inc.php
@@ -1,6 +1,13 @@
 <?php
 
-// ACLs
+/* ACLs
+ * 
+ * Remarks:
+ *
+ * user_edit_himself is only used to show the menu enty for a user to edit his own settings
+ *
+ */
+
 define ("ACL",serialize(array(
                           "systemadmin" => array("main","domains_list","domain_new","domain_new.mailstorageserver","domain_new.spamfilter","domain_edit","domain_edit.mailstorageserver","domain_edit.spamfilter","domain_delete","users_list","user_new","user_edit","user_edit.active","user_delete","alias_new","alias_edit","alias_delete","settings","systemusers_list","systemuser_edit","systemuser_delete","globaladmins_edit","statistics"),
                           "domainadmin" => array("main","domains_list","domain_edit","domain_edit.spamfilter","users_list","user_new","user_edit","user_edit.active","user_delete","alias_new","alias_edit","alias_delete","statistics"),
