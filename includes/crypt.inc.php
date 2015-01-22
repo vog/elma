072fdc767fc2c09ec6b194610d18bc9c6df5899b
Volker Grabsch <v@njh.eu>
2015-01-23 00:22:26 +0100

    Improve indentation and spacing

--- includes/crypt.inc.php
+++ includes/crypt.inc.php
@@ -39,11 +39,11 @@ class mycrypt {
      */
 
     function mycrypt () {
-	$this->key = MYCRYPT_KEY;
-	$this->cipher = MCRYPT_RIJNDAEL_256;
+        $this->key = MYCRYPT_KEY;
+        $this->cipher = MCRYPT_RIJNDAEL_256;
         $this->mode = MCRYPT_MODE_ECB;
-	$iv_size = mcrypt_get_iv_size($this->cipher, $this->mode);
-	$this->iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+        $iv_size = mcrypt_get_iv_size($this->cipher, $this->mode);
+        $this->iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
     }
  
     /**
@@ -58,7 +58,7 @@ class mycrypt {
      * decryption function
      */
     function decrypt ($text) {
-	$return_text =  mcrypt_decrypt($this->cipher, $this->key, $text, $this->mode, $this->iv);
+        $return_text = mcrypt_decrypt($this->cipher, $this->key, $text, $this->mode, $this->iv);
         return $return_text;
     } 
 }
